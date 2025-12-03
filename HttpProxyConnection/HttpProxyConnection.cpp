4db8a7daff0180177a0316ada6df2ebb65c00c5c
Eugenio Miró <em@liquidmercury.com>
2025-12-03 15:45:24 -0300

    Add project files.

---
+++ HttpProxyConnection/HttpProxyConnection.cpp
@@ -0,0 +1,12 @@
+﻿// HttpProxyConnection.cpp : Defines the entry point for the application.
+//
+
+#include "HttpProxyConnection.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
